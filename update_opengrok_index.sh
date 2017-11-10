87ea746736d57c3f1ae9df143dff69e5e4dadbe7
lugf <luusuu@126.com>
2017-11-10 14:18:04 +0800

    add files

---
+++ update_opengrok_index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+cd /home/android/opengrok/bin
+
+SRC_PATH=$3
+export    OPENGROK_INSTANCE_BASE=$1
+export    OPENGROK_WEBAPP_CONTEXT=$2
+export 	  OPENGROK_WEBAPP_CFGADDR=localhost:$4
+export IGNORE_PATTERNS="-i *.bz2 -i *.o  -i *.a -i *.jar -i *.gz -i *.so -i *.png -i *.jpg  -i *.tar -i *.tgz -i *.zip -i d:prebuilts "
+
+echo $OPENGROK_INSTANCE_BASE
+echo $OPENGROK_WEBAPP_CONTEXT
+echo $OPENGROK_WEBAPP_CFGADDR
+echo $SRC_PATH
+
+./OpenGrok index $SRC_PATH
+cd -
